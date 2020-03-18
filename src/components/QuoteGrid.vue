7e4c04ac98959749a54fe19947e233e3d56c91dc
Carl Hain <crhain@gmail.com>
2020-03-18 12:47:38 -0500

    Create Quote and QuoteGrid components

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row">
+    <div v-for="quote in quotes">{{ quote }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuoteGrid",
+  props: ["quotes"]
+};
+</script>
+
+<style></style>
